8e57e85072fcff2668784a2debb0e5baf3ec56d5
Martin Jordanov <xxm0703@abv.bg>
2016-08-13 23:34:11 +0300

    Finished

--- morski.py
+++ morski.py
@@ -1,3 +1,4 @@
+import pdb
 import pygame
 from math import pi
 
@@ -6,7 +7,7 @@ pygame.init()
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 
-size = [400, 400]
+size = [300, 300]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Tic Tac Toe")
 
@@ -19,22 +20,82 @@ while not done:
     
     screen.fill(WHITE)
     y = 0
-    while y < 400:
+    while y < 300:
         x = 0    
-        while x < 400:
+        while x < 300:
              pygame.draw.rect(screen, BLACK, [x, y, 100, 100], 2)
              x += 100
         y += 100
     pygame.display.flip()
     b = 0
     l = []
-    while True:
+    p = []
+    while b != 3:
         a = input("Enter cell: ")
-        if l.count(a) == 0:
+        if b == 0:
             l.append(a)
-        else:
-            print "Sorry, that's not possible!"
+            if 1.3 in l and 1.2 in l and 1.1 in l:
+                print "'X' WIN!"
+                b = 3
+            if 2.3 in l and 2.2 in l and 2.1 in l:
+                print "'X' WIN!"
+                b = 3
+            if 3.3 in l and 3.2 in l and 3.1 in l:
+                print "'X' WIN!"
+                b = 3
+            if 1.3 in l and 2.3 in l and 3.3 in l:
+                print "'X' WIN!"
+                b = 3
+            if 1.2 in l and 2.2 in l and 3.2 in l:
+                print "'X' WIN!"
+                b = 3
+            if 1.1 in l and 2.1 in l and 3.1 in l:
+                print "'X' WIN!"
+                b = 3
+            if 1.1 in l and 2.2 in l and 3.3 in l:
+                print "'X' WIN!"
+                b = 3
+            if 3.1 in l and 2.2 in l and 1.3 in l:
+                print "'X' WIN!"
+                b = 3
             
+        elif b == 1:
+            p.append(a)
+            if 1.3 in p and 1.2 in p and 1.1 in p:
+                print "'O' WIN!"
+                b = 3
+            if 2.3 in p and 2.2 in p and 2.1 in p:
+                print "'O' WIN!"
+                b = 3
+            if 3.3 in p and 3.2 in p and 3.1 in p:
+                print "'O' WIN!"
+                b = 3
+            if 1.3 in p and 2.3 in p and 3.3 in p:
+                print "'O' WIN!"
+                b = 3
+            if 1.2 in p and 2.2 in p and 3.2 in p:
+                print "'O' WIN!"
+                b = 3
+            if 1.1 in p and 2.1 in p and 3.1 in p:
+                print "'O' WIN!"
+                b = 3
+            if 1.1 in p and 2.2 in p and 3.3 in p:
+                print "'O' WIN!"
+                b = 3
+            if 3.1 in p and 2.2 in p and 1.3 in p:
+                print "'O' WIN!"
+                b = 3
+        g = p + l
+        if len(g) == 9:
+            print "Game Over!"
+            break
+        if g.count(a) > 1 :
+            print "Sorry, that's not possible!"
+            if b == 1:
+                p.pop()
+            else:
+                l.pop()
+            continue
         if b == 0:
             if a == 1.1:
                 pygame.draw.line(screen, BLACK, [2,2], [98, 98], 3)
@@ -71,10 +132,11 @@ while not done:
             elif a == 3.3:
                 pygame.draw.line(screen, BLACK, [202,202], [298, 298], 3)
                 pygame.draw.line(screen, BLACK, [298,202], [202, 298], 3)
-
+            pygame.display.flip()
             b = 1
-        
-        elif b == 1:        
+            continue
+            
+        if b == 1:        
             if a == 1.1:
                 pygame.draw.ellipse(screen, BLACK, [0, 0, 100, 100], 3)
         
@@ -103,11 +165,8 @@ while not done:
                 pygame.draw.ellipse(screen, BLACK, [200, 200, 100, 100], 3)                        
         
             b = 0
-        pygame.display.flip()    
-        
-
-
-
+            pygame.display.flip()
+            continue
      
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
