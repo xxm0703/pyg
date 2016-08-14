06ea3af55f09931b9c16c405dd2a22f68e19bc66
Martin Jordanov <xxm0703@abv.bg>
2016-08-15 00:46:43 +0300

    Replacing

--- morski.py
+++ morski.py
@@ -11,6 +11,23 @@ size = [300, 300]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Tic Tac Toe")
 
+class exes (object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def draw(self):
+        pygame.draw.line(screen, BLACK, [self.x+6,self.y+6], [self.x+94, self.y+94], 3)
+        pygame.draw.line(screen, BLACK, [self.x+94,self.y+6], [self.x+6, self.y+94], 3)    
+ 
+class circles(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def draw(self):
+        pygame.draw.ellipse(screen, BLACK, [self.x+6, self.y+6, 88, 88], 3)                   
+
 done = False
 clock = pygame.time.Clock()
 
@@ -23,7 +40,7 @@ while not done:
     while y < 300:
         x = 0    
         while x < 300:
-             pygame.draw.rect(screen, BLACK, [x, y, 100, 100], 2)
+             pygame.draw.rect(screen, BLACK, [x, y, 100, 100], 1)
              x += 100
         y += 100
     pygame.display.flip()
@@ -98,71 +115,62 @@ while not done:
             continue
         if b == 0:
             if a == 1.1:
-                pygame.draw.line(screen, BLACK, [2,2], [98, 98], 3)
-                pygame.draw.line(screen, BLACK, [98,2], [2, 98], 3)
+                exes(0, 0).draw()
                     
             elif a == 1.2:
-                pygame.draw.line(screen, BLACK, [102,2], [198,98], 3)
-                pygame.draw.line(screen, BLACK, [198,2], [102, 98], 3)
+                exes(100, 0).draw()
                     
             elif a == 1.3:
-                pygame.draw.line(screen, BLACK, [202,2], [298, 98], 3)
-                pygame.draw.line(screen, BLACK, [298,2], [202, 98], 3)
+                exes(200, 0).draw()
                     
             elif a == 2.1:
-                pygame.draw.line(screen, BLACK, [2,102], [98, 198], 3)
-                pygame.draw.line(screen, BLACK, [98,102], [2, 198], 3)
+                exes(0, 100).draw()
                  
             elif a == 2.2:
-                pygame.draw.line(screen, BLACK, [102,102], [198, 198], 3)
-                pygame.draw.line(screen, BLACK, [198,102], [102, 198], 3)
+                exes(100, 100).draw()
                     
             elif a == 2.3:
-                pygame.draw.line(screen, BLACK, [202,102], [298, 198], 3)
-                pygame.draw.line(screen, BLACK, [298,102], [202, 198], 3)
+                exes(200, 100).draw()
             
             elif a == 3.1:
-                pygame.draw.line(screen, BLACK, [2,202], [98, 298], 3)
-                pygame.draw.line(screen, BLACK, [98,202], [2, 298], 3)
+                exes(0, 200).draw()
             
             elif a == 3.2:
-                pygame.draw.line(screen, BLACK, [102,202], [198, 298], 3)
-                pygame.draw.line(screen, BLACK, [198,202], [102, 298], 3)
+                exes(100, 200).draw()
             
             elif a == 3.3:
-                pygame.draw.line(screen, BLACK, [202,202], [298, 298], 3)
-                pygame.draw.line(screen, BLACK, [298,202], [202, 298], 3)
+                exes(200, 200).draw()
             pygame.display.flip()
             b = 1
             continue
             
         if b == 1:        
             if a == 1.1:
-                pygame.draw.ellipse(screen, BLACK, [0, 0, 100, 100], 3)
+                circles(0,0).draw()
         
             elif a == 1.2:        
-                pygame.draw.ellipse(screen, BLACK, [100, 0, 100, 100], 3)                
+                circles(100,0).draw()
 
             elif a == 1.3:        
-                pygame.draw.ellipse(screen, BLACK, [200, 0, 100, 100], 3)
+                circles(200,0).draw()
                 
             elif a == 2.1:        
-                pygame.draw.ellipse(screen, BLACK, [0, 100, 100, 100], 3)                
+                circles(0,100).draw()
                 
             elif a == 2.2:        
-                pygame.draw.ellipse(screen, BLACK, [100, 100, 100, 100], 3)
+                circles(100,100).draw()
                        
             elif a == 2.3:        
-                pygame.draw.ellipse(screen, BLACK, [200, 100, 100, 100], 3)                       
+                circles(200,100).draw()
                        
             elif a == 3.1:        
-                pygame.draw.ellipse(screen, BLACK, [0, 200, 100, 100], 3)                       
+                circles(0,200).draw()
                        
             elif a == 3.2:        
-                pygame.draw.ellipse(screen, BLACK, [100, 200, 100, 100], 3)                       
+                circles(100,200).draw()
                         
             elif a == 3.3:        
-                pygame.draw.ellipse(screen, BLACK, [200, 200, 100, 100], 3)                        
+                circles(200,200).draw() 
         
             b = 0
             pygame.display.flip()
