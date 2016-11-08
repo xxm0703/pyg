0a9e93ac6ce8905701cb091e6eedc6fa0879c35c
xxm0703 <xxm0703@abv.bg>
2016-11-08 22:35:14 +0200

    physics_complete

--- snake_test.py
+++ snake_test.py
@@ -1,19 +1,22 @@
-import pygame, random
+import pygame
+import random
 pygame.init()
 
 wide = 800
 high = 600
-snake_x = wide/2 # To be at the center of the screen
+snake_x = wide/2  # To be at the center of the screen
 snake_y = high/2
-apple_x = random.randint(10, wide - 10) # Some space before border
+apple_x = random.randint(10, wide - 10)  # Some space before border
 apple_y = random.randint(10, high - 10)
+print(apple_x, apple_y)
 
 RED = [255, 0, 0]
 GREEN = [0, 255, 0]
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
 
-def message(msg, color = BLACK, size = 25, line = 1):
+
+def message(msg, color=BLACK, size=25, line=1):
     font = pygame.font.SysFont(None, size, True)
     text = font.render(msg, True, color)
     screen.blit(text, [wide/2 - len(msg)*6, high/2 + line * 25])
@@ -26,15 +29,10 @@ PLAY = True
 in_game = False
 change_x = 0
 change_y = -10
-right_eye_x = 0
-right_eye_y = 0
-left_eye_x = 0
-left_eye_y = 0
-lenght = 10
-side = 10
 apple_count = 0
 pos_list = []
-pos_list.insert(0,(snake_x, snake_y))
+pos_list.insert(0, (snake_x, snake_y))
+print(pos_list)
 screen.fill(WHITE)
 while PLAY:
     message("This is my 'Snake' game. I hope you like it.", BLACK, 30)
@@ -45,7 +43,6 @@ while PLAY:
         if event.type == pygame.KEYDOWN:
             snake_x = wide / 2
             snake_y = high / 2
-            lenght = 10
             in_game = True
         if event.type == pygame.QUIT:
             PLAY = False
@@ -59,7 +56,6 @@ while PLAY:
                 if event.key == pygame.K_LEFT:
                     change_x = -10
                     change_y = 0
-
                 elif event.key == pygame.K_RIGHT:
                     change_x = 10
                     change_y = 0
@@ -72,36 +68,28 @@ while PLAY:
 
         snake_x += change_x
         snake_y += change_y
-        right_eye_x = snake_x + 2
-        right_eye_y = snake_y + 2
-        left_eye_x = snake_x + 2
-        left_eye_y = snake_y + 7
+        pos_list.insert(0, (snake_x, snake_y))
+        pos_list.pop(-1)
 
         if -10 < apple_x - snake_x < 10 and -10 < apple_y - snake_y < 10:
-            lenght += 10
-            apple_count += 1
-            message(str(apple_count), BLACK, 25, 10)
-            pygame.display.update()
             apple_x = random.randint(10, wide - 10)
-            apple_y = random.randint(10, wide - 10)
-            if apple_count == 10:
-                message("You win!", GREEN, 35)
+            apple_y = random.randint(10, high - 10)
+            pos_list.append((snake_x - change_x, snake_y - change_y))
+            if len(pos_list) == 10:
+                message("You WIN!")
+                in_game = False
         if snake_x < 0 or snake_y < 0 or snake_x > wide or snake_y > high:
             message("Game Over!", RED, 35)
             pygame.display.update()
             pygame.time.delay(2000)
             in_game = False
-        else:
-            del pos_list[-1]
-
-        pos_list.insert(0,(snake_x, snake_y))
 
         screen.fill(WHITE)
 
         pygame.draw.rect(screen, RED, [apple_x, apple_y, 9, 9])
-        pygame.draw.rect(screen, GREEN, [snake_x, snake_y, lenght, side])
-        pygame.draw.rect(screen, BLACK, [right_eye_x, right_eye_y, 2, 2])
-        pygame.draw.rect(screen, BLACK, [left_eye_x, left_eye_y, 2, 2])
+        for cord in pos_list:
+            pygame.draw.rect(screen, GREEN, [cord[0], cord[1], 10, 10])
+            print(cord[0], cord[1])
         pygame.draw.rect(screen, BLACK, [0, 0, 800, 600], 2)
         pygame.display.update()
         clock.tick(10)
